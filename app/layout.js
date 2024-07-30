35b983c3355f718162178036a69ab1f095fd6fb3
razaali333 <raza.aursoft@gmail.com>
2024-07-30 17:42:54 +0400

    Update Adsense API

--- app/layout.js
+++ app/layout.js
@@ -42,7 +42,7 @@ export default function RootLayout({ children }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>{metadata.title}</title>
 				<meta name="description" content={metadata.description} />
-				<AdSense pId="pub-1004611093175762"/>
+				<AdSense pId="ca-pub-1004611093175762"/>
 				<Script
 					strategy="lazyOnload"
 					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
