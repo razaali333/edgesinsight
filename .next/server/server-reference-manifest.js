3d59d5852018fce603424111ef77f70f68701ea7
razaali333 <raza.aursoft@gmail.com>
2024-07-30 17:38:04 +0400

    google adense and modify config nextjs

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"M+uvH34W59IriSeeIdHtoakuCFSJcCP57EVLYduatco=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lijUyiCMa4gU79Xo2CcWpg6cKgBW7Bl5xAjGgJd8c/0=\"\n}"
--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com", "res.cloudinary.com"]
+      domains: [
+        "lh3.googleusercontent.com",
+        "firebasestorage.googleapis.com",
+        "res.cloudinary.com"
+      ]
     },
     experimental: {
-        missingSuspenseWithCSRBailout: false,
+      missingSuspenseWithCSRBailout: false,
     },
-}
-
-module.exports = nextConfig
+    async headers() {
+      return [
+        {
+          source: '/api/(.*)',
+          headers: [
+            { key: 'Content-Type', value: 'application/json' },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
